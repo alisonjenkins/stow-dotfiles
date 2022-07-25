86b2cc9f10200475333df4b3815d49bbca4890a3
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-07-25 19:20:39 +0100

    Disable gitsigns null-ls integration
    Lightbulbs! Lightbulbs Everywhere!!!!!!

--- neovim/.config/nvim/lua/plugin-config/null-ls.lua
+++ neovim/.config/nvim/lua/plugin-config/null-ls.lua
@@ -1,6 +1,5 @@
 local null_ls = require("null-ls")
 local lSsources = {
-  null_ls.builtins.code_actions.gitsigns,
   null_ls.builtins.formatting.terraform_fmt,
   null_ls.builtins.formatting.prettierd.with({
     filetypes = {
