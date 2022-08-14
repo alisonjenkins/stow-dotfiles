b837b97c1df3c6f8ff96cefc0528d9494522c20f
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-08-14 08:26:19 +0100

    Convert Nvim options over to lua where possible
    A few remain that cannot be moved over to lua =(.

--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -1,43 +1,46 @@
-vim.o.backupdir = vim.fn.stdpath("data") .. "/backup" -- set backup directory to be a subdirectory of data to ensure that backups are not written to git repos
-vim.o.directory = vim.fn.stdpath("data") .. "/directory" -- Configure 'directory' to ensure that Neovim swap files are not written to repos.
-vim.o.undodir = vim.fn.stdpath("data") .. "/undo" -- set undodir to ensure that the undofiles are not saved to git repos.
-
 -- ensure the above directories exist
 vim.loop.fs_mkdir(vim.o.backupdir, 750)
 vim.loop.fs_mkdir(vim.o.directory, 750)
 vim.loop.fs_mkdir(vim.o.undodir, 750)
 
+vim.o.backup = true -- enable backups so if Neovim crashes or you lose power you do not lose your work.
+vim.o.backupdir = vim.fn.stdpath("data") .. "/backup" -- set backup directory to be a subdirectory of data to ensure that backups are not written to git repos
+vim.o.cmdheight = 2 -- More space for displaying messages
+vim.o.completeopt = "menu,menuone,noselect" -- Completeopt setting recommended by nvim-cmp docs.
+vim.o.conceallevel = 0 -- So that I can see `` in markdown files
+vim.o.cursorline = true -- Enable highlighting of the current line
+vim.o.directory = vim.fn.stdpath("data") .. "/directory" -- Configure 'directory' to ensure that Neovim swap files are not written to repos.
+vim.o.expandtab = true -- Converts tabs to spaces
+vim.o.foldclose = true
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.guioptions = "gmrLT" -- Use showtabline in gui vim
+vim.o.ignorecase = true -- ignore case makes searching case insensitive by default. Overridable by using a capital letter thanks to smart case.
+vim.o.laststatus = 3 -- only show one shared statusline
+vim.o.mouse = "a" -- Enable your mouse
+vim.o.number = true -- set numbered lines
+vim.o.pumheight = 0 -- Sets popup menu height to use available screen space
+vim.o.relativenumber = true -- set relative numbered lines
+vim.o.sessionoptions = vim.o.sessionoptions .. ",globals"
+vim.o.showtabline = 1 -- Show tabline when there is more than 1 tab.
+vim.o.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+vim.o.smartcase = true -- smartcase makes it so that searching becomes case sensitive if you use a capital letter in the search.
+vim.o.spell = true -- enable spell checking
+vim.o.sw = 2 -- Set shiftwidth
+vim.o.t_Co = 256 -- Support 256 colors
+vim.o.termguicolors = true -- set term gui colors most terminals support this
+vim.o.timeoutlen = 300 -- By default timeoutlen is 1000 ms
+vim.o.ts = 2 -- Set tabstop
+vim.o.undodir = vim.fn.stdpath("data") .. "/undo" -- set undodir to ensure that the undofiles are not saved to git repos.
+vim.o.undofile = true -- enable persistent undo (meaning if you quit Neovim and come back to a file and want to undo previous changes you can)
+vim.o.updatetime = 300 -- Faster completion
+vim.o.wrap = false -- Display long lines as just one line
+vim.o.writebackup = true -- enable writing of backup files when saving changes.
+
 vim.cmd([[
 let g:loaded_perl_provider=0           " Disable Perl provider in Neovim
-set backup                             " enable backups so if Neovim crashes or you lose power you do not lose your work.
-set cmdheight=2                        " More space for displaying messages
-set colorcolumn=99999                  " fix indentline for now
-set completeopt=menu,menuone,noselect  " Completeopt setting recommended by nvim-cmp docs.
-set conceallevel=0                     " So that I can see `` in markdown files
-set cursorline                         " Enable highlighting of the current line
-set expandtab                          " Converts tabs to spaces
-set guioptions-=e                      " Use showtabline in gui vim
-set hidden                             " Required to keep multiple buffers open multiple buffers
-set ignorecase                         " ignore case makes searching case insensitive by default. Overridable by using a capital letter thanks to smart case.
-set laststatus=3                       " only show one shared statusline
-set mouse=a                            " Enable your mouse
-set nowrap                             " Display long lines as just one line
-set number                             " set numbered lines
-set pumheight=10                       " Makes popup menu smaller
-set relativenumber                     " set relative numbered lines
-set sessionoptions+=tabpages,globals   " store tabpages and globals in session
-set showtabline=2                      " Always show tabs
-set signcolumn=yes                     " Always show the signcolumn, otherwise it would shift the text each time
-set smartcase                          " smartcase makes it so that searching becomes case sensitive if you use a capital letter in the search.
-set spell                              " enable spell checking
-set sw=2                               " Set shiftwidth
-set t_Co=256                           " Support 256 colors
-set termguicolors                      " set term gui colors most terminals support this
-set timeoutlen=300                     " By default timeoutlen is 1000 ms
-set ts=2                               " Set tabstop
-set undofile                           " enable persistent undo (meaning if you quit Neovim and come back to a file and want to undo previous changes you can)
-set updatetime=300                     " Faster completion
-set writebackup                        " enable writing of the backup files
+" set colorcolumn=99999                  " fix indentline for now
 
 try
 colorscheme kanagawa
