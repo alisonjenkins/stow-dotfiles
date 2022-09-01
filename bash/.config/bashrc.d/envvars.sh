322c42611a8bf2d0c67fbbb25dd27fd30c20961d
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-09-01 21:29:55 +0100

    Add cargo bin path to the PATH var on zsh and bash

--- bash/.config/bashrc.d/envvars.sh
+++ bash/.config/bashrc.d/envvars.sh
@@ -5,24 +5,24 @@ export EDITOR="nvim"
 export GOPATH="$HOME/go"
 export HISTCONTROL=ignoredups:ignorespace
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 export PATH="$PATH:$HOME/.gem/ruby/2.4.0/bin"
 export PATH="$PATH:$HOME/.gem/ruby/2.5.0/bin"
 export PATH="$PATH:$HOME/.gem/ruby/2.6.0/bin"
 export PATH="$PATH:$HOME/.homebrew/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.yarn/bin/"
+export PATH="$PATH:$HOME//.cargo/bin"
 export PATH="$PATH:$HOME/Library/Python/3.8/bin/"
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:/usr/local/sbin"
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 # {{{ nnn settings
 export NNN_OPTS="aedF"
 export NNN_BMS="D:~/Documents;d:~/Downloads;g:~/git;h:~;"
 # }}}
 
-if [ -z "$SSH_AUTH_SOCK" ];
-then
-    export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+if [ -z "$SSH_AUTH_SOCK" ]; then
+  export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
