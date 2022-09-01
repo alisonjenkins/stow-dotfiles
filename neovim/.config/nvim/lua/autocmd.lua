fa0b00582c34eda2737b8c1193bb6a3608905cbe
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-09-01 20:43:01 +0100

    Automatically format Rust code on save

--- neovim/.config/nvim/lua/autocmd.lua
+++ neovim/.config/nvim/lua/autocmd.lua
@@ -1,1 +1,2 @@
-vim.cmd [[au TextYankPost * silent! lua vim.highlight.on_yank()]]
+vim.cmd([[au BufWritePre *.rs lua vim.lsp.buf.formatting_sync(nil, 200)]])
+vim.cmd([[au TextYankPost * silent! lua vim.highlight.on_yank()]])
