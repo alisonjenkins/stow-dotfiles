7fa525f026eb02f9838bd4c53c9a4293107ef937
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-12-25 10:23:45 +0000

    Try make neodev work

--- neovim/.config/nvim/lua/plugin-config/lspconfig.lua
+++ neovim/.config/nvim/lua/plugin-config/lspconfig.lua
@@ -1,9 +1,21 @@
 -- vim: set foldmethod=marker foldlevel=0:
 
+local neodev_ok, neodev = pcall(require, "neodev")
+if neodev_ok then
+  neodev.setup({})
+end
+
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+local lsp_defaults = lspconfig.util.default_config
+
+if not lspconfig_ok then
+  return
+end
+
 local function custom_capabilities() --{{{
-  local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-  if not ok then
-    return vim.lsp.protocol.make_client_capabilities()
+  local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+  if not cmp_nvim_lsp_ok then
+    return vim.tbl_deep_extend("force", lsp_defaults.capabilities, vim.lsp.protocol.make_client_capabilities())
   end
 
   local capabilities = cmp_nvim_lsp.default_capabilities()
@@ -29,10 +41,11 @@ end
 --}}}
 
 local function default(configs) --{{{
-  local custom_config = {
-    on_attach = custom_on_attach,
-    capabilities = custom_capabilities(),
-  }
+  -- local custom_config = {
+  --   on_attach = custom_on_attach,
+  --   capabilities = custom_capabilities(),
+  -- }
+  local custom_config = {}
   if configs ~= nil then
     for key, value in pairs(configs) do
       if value ~= nil then
@@ -91,13 +104,16 @@ lsp_servers["jsonls"] = {}
 -- }}}
 
 -- {{{ Lua
+
 lsp_servers["sumneko_lua"] = {
-  Lua = {
-    completion = {
-      callSnippet = "Replace",
-    },
-    hint = {
-      enable = true,
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace",
+      },
+      hint = {
+        enable = true,
+      },
     },
   },
 }
@@ -134,9 +150,19 @@ lsp_servers["yamlls"] = {}
 require("toggle_lsp_diagnostics").init({ underline = false, virtual_text = { spacing = 4 } })
 
 ----{{{ LSP setup
-local lspconfig = require("lspconfig")
-
 for lsp_name, lsp_settings in pairs(lsp_servers) do
-  lspconfig[lsp_name].setup(default(lsp_settings))
+  if lsp_name == "sumneko_lua" then
+    lspconfig.sumneko_lua.setup({
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = "Replace",
+          },
+        },
+      },
+    })
+  else
+    lspconfig[lsp_name].setup(default(lsp_settings))
+  end
 end
 --}}}
