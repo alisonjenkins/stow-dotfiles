17b15b49fd58962ab42b56755d505ba46e066eb7
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-12-25 07:54:28 +0000

    Configure nvim-hlslens properly

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -355,7 +355,11 @@ packer.startup(function()
   use({ "b0o/incline.nvim", config = get_plugin_config("incline") })
   -- }}}
   -- Search index overlay {{{
-  use({ "kevinhwang91/nvim-hlslens", config = get_plugin_config("nvim-hlslens"), requires = { "nvim-lua/plenary.nvim" } })
+  use({
+    "kevinhwang91/nvim-hlslens",
+    config = get_plugin_config("nvim-hlslens"),
+    requires = { "nvim-lua/plenary.nvim" },
+  })
   -- }}}
   -- Smooth scrolling {{{
   use({
