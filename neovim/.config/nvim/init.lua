ec7002ef00ec5d1d92735d6d08625daabff20ef0
Alan Jenkins <alan.james.jenkins@gmail.com>
2023-01-02 09:49:12 +0000

    Get rid of commented caching
    We don't need impatient anymore as Lazy does it.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,9 +1,3 @@
--- Caching
--- local hasimpatient, impatient = pcall(require, "impatient")
--- if hasimpatient then
-  -- impatient.enable_profile()
--- end
-
 require "plugins"
 require "options"
 require "autocmd"
