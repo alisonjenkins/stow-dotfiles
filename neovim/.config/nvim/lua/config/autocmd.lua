4645f1d77d027efea7de36b6bec1a3f1eee160f6
Alan Jenkins <alan.james.jenkins@gmail.com>
2023-01-04 11:08:18 +0000

    Fix tf files not being marked as being Terraform type

--- neovim/.config/nvim/lua/config/autocmd.lua
+++ neovim/.config/nvim/lua/config/autocmd.lua
@@ -1,2 +1,3 @@
+vim.cmd([[au BufRead *.tf silent! set ft=terraform]])
 vim.cmd([[au BufWritePre *.rs lua vim.lsp.buf.formatting_sync(nil, 200)]])
 vim.cmd([[au TextYankPost * silent! lua vim.highlight.on_yank()]])
