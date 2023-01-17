3235cf6e4cfcd5cc451cf41f94bfbf91e5a0056a
Alan Jenkins <alan.james.jenkins@gmail.com>
2023-01-17 12:39:02 +0000

    Load vscode snippets in Luasnips

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -5,6 +5,9 @@ local M = {
 	dependencies = {
 		{
 			"L3MON4D3/LuaSnip",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
 			dependencies = {
 				"rafamadriz/friendly-snippets",
 			},
