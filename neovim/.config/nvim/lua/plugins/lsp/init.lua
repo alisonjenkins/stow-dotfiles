a40fae4847420d7ec770ec3e0179508d19be5a2b
Alan Jenkins <alan.james.jenkins@gmail.com>
2023-01-17 12:40:23 +0000

    Use JDTLS LSP

--- neovim/.config/nvim/lua/plugins/lsp/init.lua
+++ neovim/.config/nvim/lua/plugins/lsp/init.lua
@@ -43,6 +43,7 @@ function M.config()
 		svelte = {},
 		eslint = {},
 		html = {},
+		jdtls = {},
 		jsonls = {
 			on_new_config = function(new_config)
 				new_config.settings.json.schemas = new_config.settings.json.schemas or {}
