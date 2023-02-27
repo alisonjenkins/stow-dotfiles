c67aff143a98970bc810add4cb631c702867f4e8
Alan Jenkins <alan.james.jenkins@gmail.com>
2023-02-27 21:44:35 +0000

    Improvements to the zsh setup
    * Switch from using asdf to rtx which is a new version manager
      compatible with the asdf ecosystem that is way faster.
    * Rework how zoxide is installed.
    * Fix Mcfly downloading the wrong release for the operating system and
      CPU architecture.

--- zsh/.config/zshrc.d/plugins.zsh
+++ zsh/.config/zshrc.d/plugins.zsh
@@ -14,7 +14,6 @@ zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 # Plugins
 zinit load Aloxaf/fzf-tab
-zinit load alanjjenkins/asdf.plugin.zsh
 zinit load alanjjenkins/kube-aliases
 zinit load fabiokiatkowski/exercism.plugin.zsh
 zinit load joepvd/zsh-hints
@@ -22,38 +21,56 @@ zinit load macunha1/zsh-terraform
 zinit load molovo/tipz
 zinit load zsh-users/zsh-completions
 
-if ! command -v zoxide &>/dev/null; then
-        if command -v &>/dev/null; then
-                cargo install zoxide
-        fi
-fi
+# Install rtx version manager (replacement for asdf)
+zinit ice from"gh-r" as"command" mv"rtx* -> rtx" \
+  atclone'rtx activate -s zsh > init.zsh; ./rtx complete -s zsh > _rtx' \
+  atpull'%atclone' src"init.zsh"
+zinit light jdxcode/rtx
 
-eval "$(zoxide init zsh)"
+# install zoxide
+zinit ice wait lucid from"gh-r" as"command" \
+  atclone'./zoxide init zsh > init.zsh' \
+  atpull'%atclone' src"init.zsh"
+zinit light ajeetdsouza/zoxide
 
 # Setup direnv
-eval "$(asdf exec direnv hook zsh)"
+eval "$(rtx exec direnv -- direnv hook zsh)"
 
 # A shortcut for asdf managed direnv.
-direnv() { asdf exec direnv "$@"; }
-
+direnv() { rtx exec direnv -- direnv "$@"; }
 
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/aws/aws.plugin.zsh'
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/command-not-found/command-not-found.plugin.zsh'
-
-# Sort completion out
-# zinit wait lucid for \
- # atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
-    # zdharma-continuum/fast-syntax-highlighting \
- # blockf \
-    # zsh-users/zsh-completions \
-# atload"!_zsh_autosuggest_start" \
-    # zsh-users/zsh-autosuggestions
 
 # For postponing loading `fzf`
 zinit ice lucid wait
 zinit snippet OMZP::fzf
 
-zinit ice lucid wait"0a" from"gh-r" as"program" atload'eval "$(mcfly init zsh)"'
+# Fix Mcfly downloading the wrong binary
+case $(uname) in
+  Darwin)
+    case $(uname -m) in
+      x86_64)
+        mcfly_os="*x86_64*darwin*"
+      ;;
+      arm64)
+        mcfly_os="*aarch64*darwin*"
+      ;;
+    esac
+  ;;
+  Linux)
+    case $(uname -m) in
+      x86_64)
+        mcfly_os="*x86_64*linux*musl*"
+      ;;
+      arm64)
+        mcfly_os="*aarch64*linux*"
+      ;;
+    esac
+  ;;
+esac
+
+zinit ice lucid wait"0a" from"gh-r" as"program" atload'eval "$(mcfly init zsh)"' bpick"${mcfly_os}"
 zinit light cantino/mcfly
 
 autoload -Uz _zinit
