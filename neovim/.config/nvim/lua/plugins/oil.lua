3fb77ab8e3b1350185d5523fb831f6cdd8329848
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-04-13 08:55:41 +0100

    Fix mappings of oil so it is not annoying!

--- neovim/.config/nvim/lua/plugins/oil.lua
+++ neovim/.config/nvim/lua/plugins/oil.lua
@@ -9,6 +9,19 @@ function M.config()
 		columns = {
 			"icon",
 		},
+		view_options = {
+			show_hidden = true,
+		},
+		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = "actions.tcd",
+			["g."] = "actions.toggle_hidden",
+		},
+		use_default_keymaps = false,
 		skip_confirm_for_simple_edits = true,
 	})
 	vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
