3b51ea3c813a38c1cd951ce08f8aa68f541ad6db
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-06-02 14:53:46 +0100

    Configure Gitsigns to do gitblame on lines

--- neovim/.config/nvim/lua/plugins/git-signs.lua
+++ neovim/.config/nvim/lua/plugins/git-signs.lua
@@ -5,7 +5,14 @@ local M = {
 }
 
 function M.config()
-  require("gitsigns").setup()
+  require("gitsigns").setup({
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "right_align",
+      delay = 300,
+    }
+  })
 end
 
 return M
