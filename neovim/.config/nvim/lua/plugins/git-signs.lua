cb9adcdaaecfce44e3185f60ab503326cb2d0a1a
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-06-05 08:32:51 +0100

    Switch to eol gitblame
    Because it handles narrow splits and windows better.

--- neovim/.config/nvim/lua/plugins/git-signs.lua
+++ neovim/.config/nvim/lua/plugins/git-signs.lua
@@ -9,7 +9,7 @@ function M.config()
     current_line_blame = true,
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = "right_align",
+      virt_text_pos = "eol",
       delay = 300,
     }
   })
