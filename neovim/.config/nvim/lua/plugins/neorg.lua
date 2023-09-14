22d73016e740fd7d4fc4ea5b7be1a84d77249f75
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-09-14 08:59:31 +0100

    Fix Neorg config

--- neovim/.config/nvim/lua/plugins/neorg.lua
+++ neovim/.config/nvim/lua/plugins/neorg.lua
@@ -2,6 +2,9 @@ local M = {
   "nvim-neorg/neorg",
   build = ":Neorg sync-parsers",
   lazy = true,
+  cmd = {
+    "Neorg",
+  },
   ft = "norg",
   dependencies = "nvim-lua/plenary.nvim",
 }
@@ -16,14 +19,27 @@ function M.config()
   neorg.setup({
     -- Tell Neorg what modules to load
     load = {
-      ["core.defaults"] = {}, -- Load all the default modules
+      ["core.defaults"] = {},  -- Load all the default modules
+      ["core.ui.calendar"] = {},
       ["core.concealer"] = {}, -- Allows for use of icons
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+          name = "[Neorg]",
+        }
+      },
       ["core.dirman"] = { -- Manage your directories with Neorg
         config = {
           workspaces = {
-            work = "~/Documents/gtd/work",
+            personal = "~/Documents/Notes/Personal",
+            work = "~/Documents/Notes/Work",
           },
         },
+      },
+      ["core.summary"] = {
+        config = {
+          strategy = "default",
+        }
       },
       -- ["core.gtd.base"] = {
       --   config = {
