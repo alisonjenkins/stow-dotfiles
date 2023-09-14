faa546e5825330c273e128fce1347331564f103c
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-09-14 09:00:04 +0100

    Add navigator config to lsp-zero

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -69,5 +69,54 @@ return {
       },
     })
 
+    require 'navigator'.setup({
+      preview_height = 0.35,        -- max height of preview windows
+      ts_fold = true,               -- modified version of treesitter folding
+      default_mapping = false,      -- set to false if you will remap every key or if you using old version of nvim-
+      treesitter_analysis = true,   -- treesitter variable context
+      treesitter_navigation = true, -- bool|table false: use lsp to navigate between symbol ']r/[r', table: a list of
+      treesitter_analysis_max_num = 100,
+      treesitter_analysis_condense = true,
+      transparency = 50,
+      lsp_signature_help = true, -- if you would like to hook ray-x/lsp_signature plugin in navigator
+      -- setup here. if it is nil, navigator will not init signature help
+      signature_help_cfg = nil,  -- if you would like to init ray-x/lsp_signature plugin in navigator, and pass in your own config to signature help
+      icons = {
+        -- Code action
+        code_action_icon = "🏏", -- note: need terminal support, for those not support unicode, might crash
+        -- Diagnostics
+        diagnostic_head = '🐛',
+        diagnostic_head_severity_1 = "🈲",
+        -- refer to lua/navigator.lua for more icons setups
+      },
+      mason = true,    -- set to true if you would like use the lsp installed by williamboman/mason
+      lsp = {
+        enable = true, -- skip lsp setup, and only use treesitter in navigator.
+        -- Use this if you are not using LSP servers, and only want to enable treesitter support.
+        -- If you only want to prevent navigator from touching your LSP server configs,
+        -- use `disable_lsp = "all"` instead.
+        -- If disabled, make sure add require('navigator.lspclient.mapping').setup({bufnr=bufnr, client=client}) in your
+        -- own on_attach
+        code_action = { enable = true, sign = true, sign_priority = 40, virtual_text = true },
+        code_lens_action = { enable = true, sign = true, sign_priority = 40, virtual_text = true },
+        document_highlight = true, -- LSP reference highlight,
+        -- it might already supported by you setup, e.g. LunarVim
+        format_on_save = false,    -- {true|false} set to false to disasble lsp code format on save (if you are using prettier/efm/formater etc)
+        -- table: {enable = {'lua', 'go'}, disable = {'javascript', 'typescript'}} to enable/disable specific language
+        -- enable: a whitelist of language that will be formatted on save
+        -- disable: a blacklist of language that will not be formatted on save
+        -- function: function(bufnr) return true end to enable/disable lsp format on save
+        format_options = { async = true }, -- async: disable by default, the option used in vim.lsp.buf.format({async={true|false}, name = 'xxx'})
+        diagnostic = {
+          underline = true,
+          virtual_text = true, -- show virtual for diagnostic message
+          update_in_insert = false, -- update diagnostic message in insert mode
+        },
+        diagnostic_scrollbar_sign = { '▃', '▆', '█' }, -- experimental:  diagnostic status in scroll bar area; set to false to disable the diagnostic sign,
+        diagnostic_virtual_text = true, -- show virtual for diagnostic message
+        diagnostic_update_in_insert = false, -- update diagnostic message in insert mode
+        display_diagnostic_qf = true, -- always show quickfix if there are diagnostic errors, set to false if you want to ignore it
+      }
+    })
   end,
 }
