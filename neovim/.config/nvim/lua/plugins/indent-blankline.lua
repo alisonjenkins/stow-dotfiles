eeff199655ecad59eb920388e6aa905edbe0a616
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-10-03 08:22:32 +0100

    Fix indent blankline

--- neovim/.config/nvim/lua/plugins/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,22 +1,15 @@
 local M = {
-  "lukas-reineke/indent-blankline.nvim",
-  lazy = true,
-  event = "BufRead",
+	"lukas-reineke/indent-blankline.nvim",
+	lazy = true,
+	event = "BufRead",
+	main = "ibl",
+	opts = {},
 }
 
 function M.config()
-  local ok, indent = pcall(require, "indent_blankline")
-  if not ok then
-    return
-  end
-
-  indent.setup({
-    show_current_context = true,
-    show_current_context_start = true,
-    show_end_of_line = true,
-  })
-  vim.opt.list = true
-  vim.opt.listchars:append("eol:↴")
+	require("ibl").setup()
+	vim.opt.list = true
+	vim.opt.listchars:append("eol:↴")
 end
 
 return M
