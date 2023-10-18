d80b5bcc856117156d35ea7fd8f34b7ed2baf0d8
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-10-18 17:17:29 +0100

    Add json formatting to conform's config

--- neovim/nvim/lua/plugins/conform.lua
+++ neovim/nvim/lua/plugins/conform.lua
@@ -1,22 +1,23 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			formatters_by_ft = {
-				go = { "goimports", "golines", "gofmt", "gofumpt" },
-				javascript = { { "prettierd", "prettier" } },
-				lua = { "stylua" },
-				sh = { "shfmt" },
-				terraform = { "terraform_fmt" },
-				rust = { "rustfmt" },
-				python = { "isort", "black" },
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  opts = {},
+  config = function()
+    require("conform").setup({
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        go = { "goimports", "golines", "gofmt", "gofumpt" },
+        javascript = { { "prettierd", "prettier" } },
+        json = { { "jq" } },
+        lua = { "stylua" },
+        sh = { "shfmt" },
+        terraform = { "terraform_fmt" },
+        rust = { "rustfmt" },
+        python = { "isort", "black" },
+      },
+    })
+  end,
 }
